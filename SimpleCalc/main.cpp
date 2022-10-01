c5512fd6bab3ae9d7f60484892e5043ddf0dd00b
KornilovIgor <i9218452052@gmail.com>
2022-10-01 20:58:55 +0300

    Добавьте файлы проекта.

---
+++ SimpleCalc/main.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b;
+	cout << "Введите два числа :" << endl;
+	cin >> a >> b;
+	cout << endl << "Результаты математических и логических операций:" << endl;
+	cout << a << " + " << b << " = " << a + b << endl;
+	cout << a << " - " << b << " = " << a - b << endl;
+	cout << a << " * " << b << " = " << a * b << endl;
+	cout << a << " / " << b << " = " << 1.0 * a / b << endl;
+	cout << a << " & " << b << " = " << (a & b) << endl;
+	cout << a << " | " << b << " = " << (a | b) << endl;
+	cout << a << " ^ " << b << " = " << (a ^ b) << endl;
+
+	return 0;
+}
